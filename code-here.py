25c5fc4b866d76d18c12e12a6db4349140901c4d
joseng1109 <43773336+joseng1109@users.noreply.github.com>
2023-06-06 17:15:11 -0700

    Create code-here.py

---
+++ code-here.py
@@ -0,0 +1,14 @@
+#Start out by doing a simple Discord Bot in Python
+
+
+#Start the code off by creating a connection to Discord in your code
+
+#Next, have the your code interact with Discord's API
+
+#Easiest interaction to start out with is having your bot print a message when joining a server, like welcoming new members
+
+
+#Next you can try to programm the bot to respond to messages
+
+
+#After that, try coding a few commands for the bot to take.
